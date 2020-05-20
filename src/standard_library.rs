21b0a41ad6a4a94e5842d18db53b8db8b1022999
Sven Nilsen <bvssvni@gmail.com>
2020-05-20 21:07:26 +0200

    Added `pow(x)(0) => 1`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -194,6 +194,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app2(Mul, Any, 0.0), 0.0.into()),
         // `pow(x)(1) => x`
         Red(app2(Pow, "x", 1.0), "x".into()),
+        // `pow(x)(0) => 1`
+        Red(app2(Pow, "x", 0.0), 1.0.into()),
 
         // `add(x)(neg(y)) => sub(x)(y)`
         Red(app2(Add, "x", app(Neg, "y")), app2(Sub, "x", "y")),
