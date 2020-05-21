38a70b8bda7e7413380c7e4c4b1ce3cbb053b77f
Sven Nilsen <bvssvni@gmail.com>
2020-05-21 13:25:35 +0200

    Display format `(> _)`

--- src/expr.rs
+++ src/expr.rs
@@ -67,6 +67,8 @@ impl fmt::Display for Expr {
                     write!(w, "(< {})", b)?;
                 } else if let Sym(Rle) = **a {
                     write!(w, "(<= {})", b)?;
+                } else if let Sym(Rgt) = **a {
+                    write!(w, "(> {})", b)?;
                 } else {
                     if let Op(Compose, _, _) = **a {
                         write!(w, "({})", a)?;
