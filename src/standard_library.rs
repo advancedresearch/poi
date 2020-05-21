f7a200bd5582350066c5fcafa161c8fd830c73e9
Sven Nilsen <bvssvni@gmail.com>
2020-05-22 01:30:34 +0200

    Added `or . ((<= x), (>= x)) => \true`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -305,6 +305,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(comp(Or, (app(Rlt, "x"), app(Rge, "x"))), true.into()),
         // `or . ((<= x), (> x)) => \true`
         Red(comp(Or, (app(Rle, "x"), app(Rgt, "x"))), true.into()),
+        // `or . ((<= x), (>= x)) => \true`
+        Red(comp(Or, (app(Rle, "x"), app(Rge, "x"))), true.into()),
 
         // `d(!\x)(x) => 1`
         Red(app2(D, not_ret_var("x"), "x"), 1.0.into()),
