b916f34f3854daa094cd7176b2bd0a170da15535
Sven Nilsen <bvssvni@gmail.com>
2020-05-23 15:13:09 +0200

    Compute `rangem`
    - Display format `compute::rangem`

--- src/lib.rs
+++ src/lib.rs
@@ -862,6 +862,8 @@ impl Context {
                                       else {Ret(Bool(false))},
                             Ranger => if c > a && c <= b {Ret(Bool(true))}
                                       else {Ret(Bool(false))},
+                            Rangem => if c > a && c < b {Ret(Bool(true))}
+                                      else {Ret(Bool(false))},
                             _ => return Err(Error::InvalidComputation)
                         })
                     }
