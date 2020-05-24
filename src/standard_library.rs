fbae5d1eb831c53c579907f1b08300395a853de5
Sven Nilsen <bvssvni@gmail.com>
2020-05-24 21:53:49 +0200

    Added `cos(tau) => 1`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -290,6 +290,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app2(Pow, "x", 0.0), 1.0.into()),
         // `sin(tau) => 0`
         Red(app(Sin, Tau), 0.0.into()),
+        // `cos(tau) => 1`
+        Red(app(Cos, Tau), 1.0.into()),
 
         // `add(x)(neg(y)) => sub(x)(y)`
         Red(app2(Add, "x", app(Neg, "y")), app2(Sub, "x", "y")),
