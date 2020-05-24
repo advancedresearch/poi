974b9d25fbf2ea9acd22526049f4ab5048f9797e
Sven Nilsen <bvssvni@gmail.com>
2020-05-24 05:49:09 +0200

    Added arity for `Neg`

--- src/arity.rs
+++ src/arity.rs
@@ -4,7 +4,7 @@ impl Symbol {
     /// Returns the arity of a symbol.
     pub fn arity(&self) -> Option<usize> {
         match self {
-            VecType | Not | Idb | Id => Some(1),
+            VecType | Not | Idb | Id | Neg => Some(1),
             Eq | Eqb | And | Or | Nand | Nor | Xor | Exc => Some(2),
             _ => None
         }
