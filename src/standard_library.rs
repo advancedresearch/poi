c9cdf1789d43cf8cf9532bd1154bec58b4381872
Sven Nilsen <bvssvni@gmail.com>
2020-05-24 05:59:06 +0200

    Fixed `(g . f:[arity]1){_}(a) <=> f[g](g(a))`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -686,8 +686,8 @@ pub fn std() -> Vec<Knowledge> {
         // `(g . f){_}(a){_}(b) <=> f[g](g(a))(g(b))`
         Eqv(app(constr(app(constr(comp("g", "f"), Any), "a"), Any), "b"),
             app(app(path("f", "g"), app("g", "a")), app("g", "b"))),
-        // `(g . f){_}(a) <=> f[g](g(a))`
-        Eqv(app(constr(comp("g", "f"), Any), "a"), app(path("f", "g"), app("g", "a"))),
+        // `(g . f:[arity]1){_}(a) <=> f[g](g(a))`
+        Eqv(app(constr(comp("g", arity_var("f", 1)), Any), "a"), app(path("f", "g"), app("g", "a"))),
         // `(g . f:[arity]2)(a)(b) <=> f[g](g(a))(g(b))`
         Eqv(app(app(comp("g", arity_var("f", 2)), "a"), "b"), app2(path("f", "g"), app("g", "a"), app("g", "b"))),
         // `g . f:[arity]2 <=> f[g] . (g . fst, g . snd)`
