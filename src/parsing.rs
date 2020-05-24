be4cb5c86f9625a6c7a5348c9c38e41cf354d417
Sven Nilsen <bvssvni@gmail.com>
2020-05-24 17:40:14 +0200

    Parse `<num> tau`

--- src/parsing.rs
+++ src/parsing.rs
@@ -141,6 +141,9 @@ fn parse_expr(node: &str, mut convert: Convert, ignored: &mut Vec<Range>) -> Res
         } else if let Ok((range, val)) = convert.meta_f64("num_pi") {
             convert.update(range);
             expr = Some(app2(Mul, val, Pi));
+        } else if let Ok((range, val)) = convert.meta_f64("num_tau") {
+            convert.update(range);
+            expr = Some(app2(Mul, val, Tau));
         } else {
             let range = convert.ignore();
             convert.update(range);
