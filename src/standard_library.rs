7668d1ca2376a9024892eaf082cdca316f73c341
Sven Nilsen <bvssvni@gmail.com>
2020-05-28 18:48:55 +0200

    Added `mul[type_of](f64)(f64) => f64`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -104,6 +104,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app2(path(Add, TypeOf), F64Type, F64Type), F64Type.into()),
         // `sub[type_of](f64)(f64) => f64`
         Red(app2(path(Sub, TypeOf), F64Type, F64Type), F64Type.into()),
+        // `mul[type_of](f64)(f64) => f64`
+        Red(app2(path(Mul, TypeOf), F64Type, F64Type), F64Type.into()),
 
         // `not . not <=> idb`
         Red(comp(Not, Not), Idb.into()),
