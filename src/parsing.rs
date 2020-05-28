d813bfaa6c48763073b628ae998aa42200679d64
Sven Nilsen <bvssvni@gmail.com>
2020-05-28 18:35:28 +0200

    Added symbol `f64`

--- src/parsing.rs
+++ src/parsing.rs
@@ -136,6 +136,7 @@ fn parse_expr(node: &str, mut convert: Convert, ignored: &mut Vec<Range>) -> Res
                 "eps" | "ε" => Eps,
                 "type_of" => TypeOf,
                 "bool" => BoolType,
+                "f64" => F64Type,
                 _ => Var(val),
             }));
         } else if let Ok((range, val)) = convert.meta_bool("bool") {
