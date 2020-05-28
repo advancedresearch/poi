bd5b2b7c2717c137bac65ea219995bfba128314c
Sven Nilsen <bvssvni@gmail.com>
2020-05-28 19:29:04 +0200

    Added `exp[type_of](f64) => f64`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -106,6 +106,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app(path(Log2, TypeOf), F64Type), F64Type.into()),
         // `log10[type_of](f64) => f64`
         Red(app(path(Log10, TypeOf), F64Type), F64Type.into()),
+        // `exp[type_of](f64) => f64`
+        Red(app(path(Exp, TypeOf), F64Type), F64Type.into()),
 
         // `eq[type_of](bool)(bool) => bool`
         Red(app2(path(Eq, TypeOf), BoolType, BoolType), BoolType.into()),
