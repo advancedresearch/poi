a99d44cb457f51d4c38e8926f536cbdf4e507857
Sven Nilsen <bvssvni@gmail.com>
2020-05-28 21:46:34 +0200

    Added `pow(imag)(\2) => \-1`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -245,6 +245,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app(Tan, app2(Mul, ret_int_var("x"), Tau)), app(Tan, Tau)),
         // `pow(eps)(\2) => \0`
         Red(app2(Pow, Eps, 2.0), 0.0.into()),
+        // `pow(imag)(\2) => \-1`
+        Red(app2(Pow, Imag, 2.0), (-1.0).into()),
         // `lt(\x)(\y) => \x < \y`
         Red(app2(Lt, ret_var("x"), ret_var("y")), binop_ret_var("x", "y", Lt)),
         // `le(\x)(\y) => \x <= \y`
