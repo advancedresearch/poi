8ef17da525b944504581795d69b97655e9672049
Sven Nilsen <bvssvni@gmail.com>
2020-06-03 23:36:18 +0200

    Added `mul(imag)(imag) => pow(imag)(\2)`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -245,6 +245,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app(Tan, app2(Mul, ret_int_var("x"), Tau)), app(Tan, Tau)),
         // `pow(eps)(\2) => \0`
         Red(app2(Pow, Eps, 2.0), 0.0.into()),
+        // `mul(imag)(imag) => pow(imag)(\2)`
+        Red(app2(Mul, Imag, Imag), app2(Pow, Imag, 2.0)),
         // `pow(imag)(\2) => \-1`
         Red(app2(Pow, Imag, 2.0), (-1.0).into()),
         // `lt(\x)(\y) => compute::lt(x, y)`
