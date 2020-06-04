868fea870fd7966e26f4ecd95179e59dd5976b7d
Sven Nilsen <bvssvni@gmail.com>
2020-06-04 08:09:39 +0200

    Parse `<num>imag2`

--- src/parsing.rs
+++ src/parsing.rs
@@ -162,6 +162,9 @@ fn parse_expr(node: &str, mut convert: Convert, ignored: &mut Vec<Range>) -> Res
         } else if let Ok((range, val)) = convert.meta_f64("num_imag") {
             convert.update(range);
             expr = Some(app2(Mul, val, Imag));
+        } else if let Ok((range, val)) = convert.meta_f64("num_imag2") {
+            convert.update(range);
+            expr = Some(app2(Mul, val, Imag2));
         } else {
             let range = convert.ignore();
             convert.update(range);
