71ff273da3a0a6850458a22bceb1caf7ba533f43
Sven Nilsen <bvssvni@gmail.com>
2020-06-07 02:13:21 +0200

    Published 0.13.0

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 //! a pragmatic point-free theorem prover assistant
 //!
 //! ```text
-//! === Poi Reduce 0.12 ===
+//! === Poi Reduce 0.13 ===
 //! Type `help` for more information.
 //! > and[not]
 //! and[not]
