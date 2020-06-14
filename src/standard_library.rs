6ad620df39867b0ee206712de35a0eef7524f388
Sven Nilsen <bvssvni@gmail.com>
2020-06-14 11:38:17 +0200

    Removed `(imag3 + x * imag) : quat => (x * imag + imag3) : quat`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -134,9 +134,6 @@ pub fn std() -> Vec<Knowledge> {
                     x0 * w1 + w0 * x1 + y0 * z1 - z0 * y1,
                 )
             }),
-        // `(imag3 + x * imag) : quat => (x * imag + imag3) : quat`
-        Red(typ(app2(Add, Imag3, app2(Mul, "x", Imag)), QuatType),
-            typ(app2(Add, app2(Mul, "x", Imag), Imag3), QuatType)),
         // `(x * imag2 + imag) : quat => (imag + x * imag2) : quat`
         Red(typ(app2(Add, app2(Mul, "x", Imag2), Imag), QuatType),
             typ(app2(Add, Imag, app2(Mul, "x", Imag2)), QuatType)),
