2682bfb58599e7a2045f113962a5c131ea526cdd
Sven Nilsen <bvssvni@gmail.com>
2020-06-14 10:10:03 +0200

    Removed `(imag * (x : quat)) => ((imag : quat) * (x : quat))`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -76,8 +76,6 @@ pub fn std() -> Vec<Knowledge> {
         // `((x : quat) * neg(y)) => neg(((x : quat) * y))`
         Red(app2(Mul, typ("x", QuatType), app(Neg, "y")),
             app(Neg, app2(Mul, typ("x", QuatType), "y"))),
-        // `(imag * (x : quat)) => ((imag : quat) * (x : quat))`
-        Red(app2(Mul, Imag, typ("x", QuatType)), app2(Mul, typ(Imag, QuatType), typ("x", QuatType))),
         // `((x : quat) * imag) => ((x : quat) * (imag : quat))`
         Red(app2(Mul, typ("x", QuatType), Imag), app2(Mul, typ("x", QuatType), typ(Imag, QuatType))),
         // `\s * ([x, y, z, w] : quat) => [s * x, s * y, s * z, s * w] : quat`
