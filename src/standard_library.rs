448b5333a0a60cd5e48f8b813f4dd18591748508
Sven Nilsen <bvssvni@gmail.com>
2020-06-15 14:56:19 +0200

    Removed `pow(imag2 : quat)(\2) => \-1`

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -342,8 +342,6 @@ pub fn std() -> Vec<Knowledge> {
         Red(app2(Mul, Eps, Eps), app2(Pow, Eps, 2.0)),
         // `pow(imag)(\2) => \-1`
         Red(app2(Pow, Imag, 2.0), (-1.0).into()),
-        // `pow(imag2 : quat)(\2) => \-1`
-        Red(app2(Pow, typ(Imag2, QuatType), 2.0), (-1.0).into()),
         // `pow(imag3 : quat)(\2) => \-1`
         Red(app2(Pow, typ(Imag3, QuatType), 2.0), (-1.0).into()),
         // `lt(\x)(\y) => compute::lt(x, y)`
