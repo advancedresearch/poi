f8f7ab6912661e43e61ddf0d5cdb6696b4b0d7a8
Sven Nilsen <bvssvni@gmail.com>
2020-09-19 12:10:14 +0200

    Added goal oriented theorem proving
    Closes https://github.com/advancedresearch/poi/issues/619
    
    - Added “goal <expr>”
    - Added “reset goal”
    - Added “inc depth”
    - Added “reset depth”

--- examples/poireduce.rs
+++ examples/poireduce.rs
@@ -1,11 +1,15 @@
 use poi::*;
 
+const DEFAULT_GOAL_DEPTH: u64 = 2;
+
 fn main() {
     println!("=== Poi Reduce 0.14 ===");
     println!("Type `help` for more information.");
     let ref std = std();
 
     let mut prev_expr: Option<Expr> = None;
+    let mut goal: Option<Expr> = None;
+    let mut goal_depth: u64 = DEFAULT_GOAL_DEPTH;
     loop {
         use std::io::{self, Write};
 
@@ -58,6 +62,17 @@ fn main() {
                 }
             }
             "bye" => break,
+            "reset goal" => {goal = None; println!("Poi: Goal is reset."); continue}
+            "reset depth" => {
+                goal_depth = DEFAULT_GOAL_DEPTH;
+                println!("Poi: Goal depth is reset to {}.", goal_depth);
+                continue;
+            }
+            "inc depth" => {
+                goal_depth += 1;
+                println!("Poi: Goal depth is set to {}.", goal_depth);
+                continue;
+            }
             x => {
                 // Print definitions of symbol.
                 if x.starts_with("def ") {
@@ -115,6 +130,28 @@ fn main() {
                             continue;
                         }
                     }
+                } else if x.starts_with("goal ") {
+                    match parse_str(x[5..].trim()) {
+                        Ok(mut expr) => {
+                            // Reduce expression first.
+                            loop {
+                                if let Ok((nexpr, i)) = expr.reduce(std) {
+                                    if nexpr == expr {break};
+                                    expr = nexpr;
+                                    println!("{}\n∵ {}", expr, std[i]);
+                                } else {
+                                    break;
+                                }
+                            }
+                            println!("new goal: {}", expr);
+                            goal = Some(expr);
+                            continue;
+                        }
+                        Err(err) => {
+                            println!("ERROR:\n{}", err);
+                            continue;
+                        }
+                    }
                 }
             }
         }
@@ -139,16 +176,102 @@ fn main() {
             }
         }
 
-        let equivalences = expr.equivalences(std);
-        for i in 0..equivalences.len() {
-            let j = equivalences[i].1;
-            println!("<=>  {}\n     ∵ {}", equivalences[i].0, std[j]);
+        let goal_reached = if let Some(g) = &goal {
+            if &expr == g {
+                goal = None;
+                true
+            } else {false}
+        } else {false};
+
+        if !goal_reached {
+            let mut found_any = false;
+            let equivalences = expr.equivalences(std);
+            loop {
+                let mut line = false;
+                for i in 0..equivalences.len() {
+                    let j = equivalences[i].1;
+                    let display = if let Some(g) = &goal {
+                        let mut history = vec![expr.clone()];
+                        if let Some(d) = find_goal(g, &equivalences[i].0, std, goal_depth, &mut history) {
+                            found_any = true;
+                            if line {println!("")};
+                            print!("depth: {} ", d);
+                            true
+                        } else {
+                            false
+                        }
+                    } else {false};
+                    if display {
+                        println!("<=>  {}\n     ∵ {}", equivalences[i].0, std[j]);
+                    } else {
+                        line = true;
+                        print!(".");
+                        io::stdout().flush().unwrap();
+                    }
+                }
+                if line {println!("")};
+
+                if found_any || goal.is_none() {
+                    break;
+                } else {
+                    println!("Poi: Could not find goal in {} steps (Tip: Use `inc depth`).", goal_depth);
+                    break;
+                }
+            }
+
+            if !found_any && goal.is_some() {
+                for i in 0..equivalences.len() {
+                    let j = equivalences[i].1;
+                    println!("<=>  {}\n     ∵ {}", equivalences[i].0, std[j]);
+                }
+            }
         }
 
         println!("∴ {}", expr);
+        if goal_reached {
+            println!("Q.E.D.");
+        }
 
         prev_expr = Some(expr);
     }
+}
+
+fn find_goal(
+    goal: &Expr,
+    expr: &Expr,
+    std: &[Knowledge],
+    depth: u64,
+    history: &mut Vec<Expr>
+) -> Option<u64> {
+    // Reduce expression.
+    let mut expr = expr.clone();
+    loop {
+        if let Ok((nexpr, _)) = expr.reduce(std) {
+            if nexpr == expr {break};
+            expr = nexpr;
+        } else {
+            break;
+        }
+    }
+
+    for i in 0..history.len() {
+        if &history[i] == &expr {return None};
+    }
+    history.push(expr.clone());
+
+    if goal == &expr {return Some(0)};
+
+    if depth > 0 {
+        let equivalences = expr.equivalences(std);
+        for i in 0..equivalences.len() {
+            // Add depth to the output.
+            if let Some(d) = find_goal(goal, &equivalences[i].0, std, depth - 1, history) {
+                return Some(d+1);
+            }
+        }
+    }
+
+    None
 }
 
 fn print_help() {print!("{}", include_str!("../assets/help.txt"))}
