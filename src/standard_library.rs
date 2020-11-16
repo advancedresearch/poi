711914f50ddf5d677fadc50dca4e8f77881c05f4
Sven Nilsen <bvssvni@gmail.com>
2020-11-16 02:00:51 +0100

    Compute `transpose`
    - Added “source/trans4.poi”

--- src/standard_library.rs
+++ src/standard_library.rs
@@ -549,6 +549,8 @@ pub fn std() -> Vec<Knowledge> {
         Red(app(Norm, "x"), app(Sqrt, app(Sqnorm, "x"))),
         // `is_square_mat{(: vec)}(x) => compute::is_square_mat(x)`
         Red(app(constr(IsSquareMat, app(Rty, VecType)), "x"), unop_ret_var("x", IsSquareMat)),
+        // `transpose{(: vec)}(x) => compute::transpose(x)`
+        Red(app(constr(Transpose, app(Rty, VecType)), "x"), unop_ret_var("x", Transpose)),
         // `col(\k){(: vec)}(x) => compute::col(k, x)`
         Red(app(constr(app(Col, ret_var("k")), app(Rty, VecType)), "x"),
             binop_ret_var("k", "x", Col)),
